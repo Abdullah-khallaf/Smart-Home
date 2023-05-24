7beee7f12d8713e8a3582d5b768e063552986f15
Abdullah-khallaf <abdullah.khallaf.work@gmail.com>
2023-05-24 05:28:37 +0300

    update files

--- Smart-Home/src/App.vue
+++ Smart-Home/src/App.vue
@@ -1,30 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+      <app-header />
+      <b-container>
+      <notification-sec />
+      <temp-sec />
+      <cont-sec />
+      <footer-sec />
+    <router-view/>
+    </b-container>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+<script>
+import AppHeader from '/components/AppHeader.vue'
+import ContSec from '/components/sections/ContSec.vue'
+import FooterSec from '/components/sections/FooterSec.vue'
+import NotificationSec from '/omponents/sections/NotificationSec.vue'
+import TempSec from '/components/sections/TempSec.vue'
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    NotificationSec,
+    TempSec,
+    ContSec,
+    FooterSec,
+    
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+<style lang="scss">
+
 </style>
+
